94c747a70b0a7c8b6d4d05a706bd5f4fface18ac
Zamora <jzamora1073@gmail.com>
2023-04-14 23:04:42 -0500

    revised marketsettlement

--- bo-api/settleMarket.js
+++ bo-api/settleMarket.js
@@ -3,7 +3,7 @@ let env='stage1-backoffice-api.askottentertainment.com'
 function settleMarket(side_bet_id,winner_id){
     return cy.get('@BOToken').then(BOToken => {
         return cy.request({
-            method : 'POST',
+            method : 'PUT',
             url: 'https://'+env+'/api/v1/market-settlement?site_id=4,3,7,8,9,10,11,14,15,16,98,99,17,18,19',
             headers: {
                 authorization: 'Bearer ' + BOToken
