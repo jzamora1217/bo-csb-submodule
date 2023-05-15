0d4d82ae935533dcbc0d8bf2fba3cf138b8059b3
Zamora <jzamora1073@gmail.com>
2023-05-15 15:19:32 -0500

    added csbapi

--- csb-api/csbapi.js
+++ csb-api/csbapi.js
@@ -2,8 +2,9 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const expect = chai.expect;
 chai.use(chaiHttp);
-
-let env='stage2-api.dragonbet.co.uk'
+import { faker } from '@faker-js/faker';
+const utils = require('../support/utils');
+let env='stage1-api.dragonbet.co.uk'
 
 async function loginAPI(email, password) {
     const response = await chai.request(`https://${env}`)
@@ -60,10 +61,65 @@ async function placeBetAPI(amount, freeBets,betData) {
                 }
             ]
         });
+    cy.log(JSON.stringify(response));
     expect(response).to.have.status(202);
     return response.body;
 }
 
+async function createUserAPI(timestamp) {
+    let firstName = faker.name.firstName();
+    let lastName = faker.name.lastName();
+    let day = utils.randomNumber(1, 30);
+    let month = utils.randomNumber(1, 12);
+    let year = utils.randomNumber(1930, 1995);
+
+    let phoneNumber = "+44123" + Math.floor(Math.random() * 10000000);
+    cy.wrap(firstName).as("firstName");
+    cy.wrap(lastName).as("lastName");
+    day = day.toString();
+    month = month.toString();
+    day = day.length > 2 ? day : day.padStart(2, "0");
+    month = month.length > 2 ? month : month.padStart(2, "0");
+    let dob = month + "/" + day + "/" + year;
+    cy.wrap(dob).as("dob");
+    const response = await chai
+        .request(`https://${env}`)
+        .post("/api/v3/user/register")
+        .send({
+            email: `johnny.zamora+qa${timestamp}@fansunite.com`,
+            username: firstName + lastName,
+            phone_number: phoneNumber,
+            phone_number_country_code: "GB",
+            password: "Password1234#",
+            password_confirmation: "Password1234#",
+            date_of_birth: {
+                dob_day: day,
+                dob_month: month,
+                dob_year: year,
+            },
+            first_name: firstName,
+            last_name: lastName,
+            promotions_consent: true,
+            affiliate_code: "",
+            street_name: "43 King Street",
+            city: "London",
+            country: "GB",
+            post_code: "WC2E 8JY",
+            currency: "GBP",
+            agreedTnC: true,
+            signup_code: "",
+        })
+    return response.body;
+}
+
+async function userInfoAPI(token) {
+    const response = await chai.request(`https://${env}`)
+        .get('/api/v3/auth/user/user-information')
+        .set('authorization', `Bearer ${token}`)
+    return response.body;
+}
+
+
 module.exports = {
-    loginAPI,getBetsAPI,placeBetAPI,
+    loginAPI,getBetsAPI,placeBetAPI,createUserAPI,userInfoAPI
 };
